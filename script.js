996e7d3dc038249db7887c9fa9c59b3148745e56
redwankibria <86778079+RedwanKibria@users.noreply.github.com>
2022-10-01 19:49:26 +0000

    modified javascript

--- script.js
+++ script.js
@@ -1,57 +1,105 @@
-// console.log('hello')
+/*
+  Rock Paper Scissors 🚀🔥
+*/
 
-// let titleDiv = document.getElementById('title')
-
-// console.log('before: ', titleDiv.innerText)
-
-// let msg = 'Goodbye hala'
-
-// titleDiv.innerText = msg
-// console.log('after: ', titleDiv.innerText)
-
-// titleDiv.innerHTML = msg
-// titleDiv.style.color = 'red'
-// titleDiv.style.backgroundColor = 'blue'
+const totalScore = {computerScore: 0, playerScore: 0}
 
 
-let redDiv = document.getElementById('red')
-let yellowDiv = document.getElementById('yellow')
-let greenDiv = document.getElementById('green')
 
-// redDiv.onclick = () => {
-//   console.log('User chose: Rock')
-//   //get ComputerChoice
-//   //userChoice vs computerChoice
-//   //-1, 0, 1
-//   //show everything on the screen (DOM)
-// } 
-// yellowDiv.onclick = () => console.log('You clicked Yellow')
-// greenDiv.onclick = () => console.log('You clicked Green')
-
-const squares = document.querySelectorAll('.colorSquare')
-// console.log(squares)
-
-//forEach loop
-
-const timesClicked = { 'red': 0, 'yellow': 0, 'green': 0 }
-
-squares.forEach(square => {
-  square.onclick = () => {
-    timesClicked[square.value] += 1
-    square.innerText = timesClicked[square.value]
-    // console.log(square.value)
-  }
-})
-
-
-function clearScore() {
-  timesClicked.red = 0
-  timesClicked.yellow = 0
-  timesClicked.green = 0
-  squares.forEach(square => {
-    square.innerText = ''
-  })
+function getComputerChoice() {
+  const rpsChoice = ['Rock', 'Paper', 'Scissors']
+  const randomNumber = Math.floor(Math.random() * 3)
+  return rpsChoice[randomNumber]
 }
 
-const clearGameBtn = document.getElementById('clear-game')
-clearGameBtn.onclick = () => clearScore()
+// ** getResult compares playerChoice & computerChoice and returns the score accordingly **
+
+function getResult(playerChoice, computerChoice) {
+  // return the result of score based on if you won, drew, or lost
+  
+  let score;
+
+  // All situations where human draws, set `score` to 0
+  if (playerChoice == computerChoice) {
+    score = 0
+
+  // All situations where human wins, set `score` to 1
+  }else if (playerChoice == 'Rock' && computerChoice == 'Scissors') {
+    score = 1
+  }else if (playerChoice == 'Paper' && computerChoice == 'Rock') {
+    score = 1
+  }else if (playerChoice == 'Scissors' && computerChoice == 'Paper') {
+    score = 1
+    // Otherwise human loses (aka set score to -1)
+  }else {
+    score = -1
+  }
+   return score
+  
+}
+
+// ** showResult updates the DOM to `You Win!` or `You Lose!` or `It's a Draw!` based on the score. Also shows Player Choice vs. Computer Choice**
+function showResult(score, playerChoice, computerChoice) {
+  
+  const resultDiv = document.getElementById('result')
+  const handsDiv = document.getElementById('hands')
+  const playerScoreDiv = document.getElementById('player-score')
+
+
+  
+  if(score == -1){
+    resultDiv.innerText = 'You lose!'
+  }else if (score == 0) {
+    resultDiv.innerText = "It's a draw!"
+  }else {
+    resultDiv.innerText = "You won!!"
+  }
+
+  handsDiv.innerText = `👨 ${playerChoice} vs 🤖 ${computerChoice}`
+}
+
+// ** Calculate who won and show it on the screen **
+function onClickRPS(playerChoice) {
+  console.log({playerChoice})
+  const computerChoice = getComputerChoice()
+  console.log({computerChoice})
+  const score = getResult(playerChoice, computerChoice)
+  totalScore['playerScore'] += score
+  console.log({score})
+  console.log(totalScore)
+  showResult(score, playerChoice, computerChoice)
+}
+
+
+// ** Make the RPS buttons actively listen for a click and do something once a click is detected **
+function playGame() {
+  // use querySelector to select all RPS Buttons
+  const rpsButtons = document.querySelectorAll('.rpsButton')
+  rpsButtons[0].onclick = () => console.log(rpsButtons[0].value)
+
+  rpsButtons.forEach(rpsButton => {
+    rpsButton.onclick = () => onClickRPS(rpsButton.value)
+  })
+
+ 
+
+  // Add a click listener to the end game button that runs the endGame() function on click
+  const endGameButton = document.getElementById('endGameButton')
+  endGameButton.onclick = () => endGame(totalScore)
+}
+
+// ** endGame function clears all the text on the DOM **
+function endGame(totalScore) {
+  totalScore['playerScore'] = 0
+  totalScore['computerScore'] = 0
+
+  const resultDiv = document.getElementById('result')
+  const handsDiv = document.getElementById('hands')
+  const playerScoreDiv = document.getElementById('player-score')
+
+  resultDiv.innerText = ''
+  handsDiv.innerText = ''
+  playerScoreDiv.innerText = ''
+}
+
+playGame()
